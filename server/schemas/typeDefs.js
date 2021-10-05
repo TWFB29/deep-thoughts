f29cd25751e68890d843e46d5bdc1fbb95873b1b
twfb <twfb29@yahoo.com>
2021-10-04 21:05:24 -0400

    21.1

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,39 +1,37 @@
-//import the gql tagged template
 const { gql } = require('apollo-server-express');
 
-//create our typeDefs
 const typeDefs = gql`
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
-      }
-      
-      type User {
-        _id: ID
-        username: String
-        email: String
-        friendCount: Int
-        thoughts: [Thought]
-        friends: [User]
-      }
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
 
-    type Query {
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
-    }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 `;
 
-//export the typeDefs
 module.exports = typeDefs;
